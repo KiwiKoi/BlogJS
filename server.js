c2fe2e228ab9d3cf9ba924d83c408f41ba9c788f
Daniel <danielo.visage@gmail.com>
2020-08-02 16:19:10 +0200

    Initial commit - laid foundations

---
+++ server.js
@@ -0,0 +1,70 @@
+"use strict";
+
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+const ejs = require("ejs");
+
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+// Connect to database
+const connection = mysql.createConnection({
+  // DB properties
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "blog",
+});
+
+connection.connect((error) => {
+  if (!!error) {
+    console.log("Error");
+  } else {
+    console.log("Connected");
+  }
+});
+
+app.use(express.static(__dirname + "/public"));
+
+// Setup view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Routes
+// Home page route
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+// Create Article page route
+app.get("/create", function (req, res) {
+  res.sendFile(__dirname + "/client/create-article.html");
+});
+// Article list page route
+app.get("/articles", (req, res) => {
+  connection.query("SELECT * FROM articles", (err, articles) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(articles);
+      res.render("articles", { data: articles });
+    }
+  });
+});
+// Individual Article page route
+app.get("/article/:id", (req, res) => {
+  connection.query(
+    `SELECT * FROM articles WHERE id = ${req.params.id}`,
+    (err, article) => {
+      if (err) throw err;
+      console.log(article);
+      res.render("article", { article });
+    }
+  );
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
